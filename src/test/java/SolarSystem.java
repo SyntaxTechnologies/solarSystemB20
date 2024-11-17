1e3b17e9ec070e78f7eda57777fe54cb0000ceaa
SyntaxTechnologies <moazzam@syntaxtechs.com>
2024-11-17 12:33:00 -0500

    added planet 1

--- src/test/java/SolarSystem.java
+++ src/test/java/SolarSystem.java
@@ -11,5 +11,11 @@ public class SolarSystem {
         Archilies.color="Red";
         Archilies.size="50meters";
         Archilies.temp="90000000";
+
+
+        planet1 slayer =new planet1();
+        slayer.color="green";
+        slayer.temp="-908k";
     }
+
 }
